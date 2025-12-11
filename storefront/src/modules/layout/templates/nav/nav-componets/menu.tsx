818a8f846e0946650523ad621a686795b68f5abc
ShehanDataEngieering <shehanp21017688@gmail.com>
2025-12-11 01:51:52 +0100

    feat: refactor Menu component; improve layout structure and align shipping/cart section for better usability

--- storefront/src/modules/layout/templates/nav/nav-componets/menu.tsx
+++ storefront/src/modules/layout/templates/nav/nav-componets/menu.tsx
@@ -16,29 +16,30 @@ export default function Menu({
   return (
     <>
       <div className="d-flex align-items-center gap-4">
-        {/* Brands/DS Section */}
-
         {/* Menu List Section */}
         <div className="menu-list-section flex-grow-1">
-          <ul className="d-flex gap-3 m-0 p-0 list-unstyled">
-            {menu_data.map((menu) => (
-              <li key={menu.id} className="position-relative">
-                <Link
-                  href={menu.link}
-                  className="text-dark text-decoration-none fw-medium"
-                >
-                  {menu.title}
-                </Link>
-                <div className="home-menu tp-submenu tp-mega-menu">
-                  <div className="row row-cols-lg-4 row-cols-sm-2 row-cols-1 gx-2 gy-2 gy-lg-0"></div>
-                </div>
-              </li>
-            ))}
+          <ul className="d-flex gap-3 m-0 p-0 list-unstyled align-items-center justify-content-between">
+            <div className="d-flex gap-3 align-items-center">
+              {menu_data.map((menu) => (
+                <li key={menu.id} className="position-relative">
+                  <Link
+                    href={menu.link}
+                    className="text-dark text-decoration-none fw-medium"
+                  >
+                    {menu.title}
+                  </Link>
+                  <div className="home-menu tp-submenu tp-mega-menu">
+                    <div className="row row-cols-lg-4 row-cols-sm-2 row-cols-1 gx-2 gy-2 gy-lg-0"></div>
+                  </div>
+                </li>
+              ))}
+            </div>
+            {/* Shipping and Cart Section right aligned to corner */}
+            <li className="position-relative align-items-center d-flex gap-3">
+              <NavRegionCurrency regions={regions} />
+              <CartButton />
+            </li>
           </ul>
-        </div>
-        <div className="brands-section flex-shrink-0">
-          <NavRegionCurrency regions={regions} />
-          <CartButton />
         </div>
       </div>
     </>
